1f22eabe23df9e327764c1cbfdffcff9fee3f2fe
ishandias1999 <51787036+ishandias1999@users.noreply.github.com>
2019-12-05 09:39:31 +0000

    Add files via upload
    This is the reviews page so whenever the user changes the rating then its automatically updated in the database. the rating has the step up of 0.5 which means they can rate the app 0.5,1.5,1..... up to 5 stars. they can also write a comment and when they press the comment button then that comment will be inserted in the reviews collection along with the rating(stars).

---
+++ Mobile/MobileAuthentication/MobileRegistration/MobileReview/Review.java
@@ -0,0 +1,22 @@
+package com.example.mobileappproject;
+
+public class Review {
+
+    private String comment;
+
+    public Review(){
+
+    }
+
+    public Review(String comment) {
+        this.comment = comment;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
