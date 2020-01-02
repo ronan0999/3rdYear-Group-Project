d4e2a6a0f0e6cb7dbd17d1fe39f95634fab9c456
ishandias1999 <51787036+ishandias1999@users.noreply.github.com>
2020-01-02 17:13:58 +0000

    Add files via upload

---
+++ Mobile/MobileAuthentication/PaymentsPage/PayPalConfig.java
@@ -0,0 +1,8 @@
+package com.example.mediaiapp;
+
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "ASUcwq8m55kGEUGoY9rkI_m-P7wMqZVLskVNzf47LHBySkGnOCiBfijEnW-J7X3H8xI1EliEpSHpK5fu";
+
+}
+
---
+++ Mobile/MobileAuthentication/PaymentsPage/payment.java
@@ -0,0 +1,22 @@
+package com.example.mediaiapp;
+
+public class payment {
+
+    private String amount;
+
+    public payment() {
+    }
+
+    public payment(String amount) {
+        this.amount = amount;
+    }
+
+    //getters and setters
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+}
